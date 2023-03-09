d9b3c1c1d4cd46c6ccff9dc5c836cfb3eb668943
17th <17th@inbox.ru>
2023-03-09 22:22:07 +0300

    ыпвыап

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+target_wagon = int(input('Вагон в который сел: '))
+number_wagon = int(input('Номер вагона: '))
+
+if number_wagon == target_wagon:
+    print('Не можем определить')
+else:
+    print(f'В поезде {target_wagon + number_wagon - 1} вагона/ов')
+
